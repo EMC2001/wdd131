0af97c411f34261ee002f612317169077ae7f756
EMC2001 <emilianocastellanos0@gmail.com>
2024-11-16 14:07:30 -0700

    temples page

--- Unit 3/gpa/gpa.js
+++
@@ -1,6 +0,0 @@
-function getGrades(inputSelector) {
-    // get grades from the input box
-    // split them into an array (String.split(','))
-    // clean up any extra spaces, and make the grades all uppercase. (Array.map())
-    // return grades
-  }
