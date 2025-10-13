cf516763da93ea90c6b84096b666e4cc3e3153fa
EMC2001 <emilianocastellanos0@gmail.com>
2025-10-13 12:12:29 -0600

    Accessibility and Design Ponder Completed

---
+++ coolpics/coolpics.js
@@ -0,0 +1,40 @@
+
+const gallery = document.querySelector('.pics');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+const menuButton = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    console.log(e.target);
+    const img = e.target;
+
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+
+menuButton.addEventListener('click', () => {
+    const navigation = document.querySelector(".nav");
+    nav.classList.toggle('hide');
+});
+          
